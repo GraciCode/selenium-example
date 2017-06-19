9c155434496ad654a6fe0a2817093a339f86e50e
Daniel Bohry <danielbohry@gmail.com>
2017-06-19 18:49:56 -0300

    First commit

---
+++ src/main/java/com/lhamacorp/automacao/demoautomacao/DemoAutomacaoApplication.java
@@ -0,0 +1,12 @@
+package com.lhamacorp.automacao.demoautomacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAutomacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAutomacaoApplication.class, args);
+	}
+}
